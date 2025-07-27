17008c6a340592a5a0287182788a9880070836ac
udhuong <ungdinhhuong@gmail.com>
2025-07-27 06:04:00 +0000

    fix: fix locale

--- frontend/src/lib/apiRequest.ts
+++ frontend/src/lib/apiRequest.ts
@@ -2,13 +2,19 @@ import {getLocale} from "next-intl/server";
 
 import axiosClient from "@/lib/axiosClient";
 
+const localeMap = {
+  en: 'en',
+  vi: 'vi-VN',
+  fr: 'fr-FR'
+}
+
 export async function apiGet<T = any>(url: string, params: any = {}): Promise<T> {
   const locale = await getLocale();
   return axiosClient.get(url, {
     ...params,
     params: {
       ...params.params,
-      locale,
+      locale: localeMap[locale as keyof typeof localeMap] || 'en',
     },
   });
 }
@@ -19,7 +25,7 @@ export async function apiPost<T = any>(url: string, data: any, params: any = {})
     ...params,
     params: {
       ...params.params,
-      locale,
+      locale: localeMap[locale as keyof typeof localeMap] || 'en',
     },
   });
 }
